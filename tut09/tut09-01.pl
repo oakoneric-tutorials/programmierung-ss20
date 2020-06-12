432b3c4d70c3093c269773e054d0d83c90cec5c0
eric <k-eric@outlook.com>
2020-06-12 17:31:42 +0200

    tut09: material

--- tut09/tut09-01.pl
+++ tut09/tut09-01.pl
@@ -5,5 +5,6 @@ listnat ([]).
 listnat ([X|XS]) :- nat(X), listnat(XS).
 sublist(Xs   , [Y|Ys]) :- nat(Y), sublist(Xs, Ys). 
 sublist(Xs   , Ys )    :- prefix(Xs, Ys). 
+
 prefix([]    , Ys )    :- listnat(Ys). 
 prefix([X|Xs], [X|Ys]) :- nat(X), prefix(Xs, Ys).
